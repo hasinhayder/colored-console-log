7a75871cb11739d2aa472e872df693acfba5a511
Hasin Hayder <countdraculla@gmail.com>
2020-06-11 13:57:14 +0600

    updated color codes

--- log.js
+++ log.js
@@ -1,15 +1,15 @@
 let log = {
     typography:'font-size:14px; padding:5px;',
     info:(msg)=>{
-        console.log(`%c${msg}`,`background: #FFDC00; color: black; ${log.typography}`)
+        console.log(`%c${msg}`,`background: #FFDC00; color: #000000; ${log.typography}`)
     },
     warn:(msg)=>{
-        console.log(`%c${msg}`,`background: #FF8C00; color: white; ${log.typography}`)
+        console.log(`%c${msg}`,`background: #FF8C00; color: #FFFFFF; ${log.typography}`)
     },
     success:(msg)=>{
-        console.log(`%c${msg}`,`background: #BADA55; color: black; ${log.typography}`)
+        console.log(`%c${msg}`,`background: #BADA55; color: #FFFFFF; ${log.typography}`)
     },
     error:(msg)=>{
-        console.log(`%c${msg}`,`background: #FF0000; color: black; ${log.typography}`)
+        console.log(`%c${msg}`,`background: #FF0000; color: #000000; ${log.typography}`)
     }
 }
